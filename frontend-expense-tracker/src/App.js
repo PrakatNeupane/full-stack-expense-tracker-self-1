b2e5720beabccb1640b8f4ab8ec1a4c46dd3a325
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-05-25 07:32:44 +1000

    frontend setup

---
+++ frontend-expense-tracker/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello </h1>
+      <Button variant='secondary'>Hey <i class="fa-solid fa-0"></i></Button>
+    </div>
+  );
+}
+
+export default App;
